004fe163cb517a4ed81686fcf862fd232812e895
Bob Chesley <bob@bobchesley.net>
2023-05-19 10:51:49 -0400

    Added security class and related dependencies to test UrlValidator method.

---
+++ src/main/java/security.java
@@ -0,0 +1,16 @@
+import java.net.MalformedURLException;
+import org.apache.commons.validator.routines.UrlValidator;
+
+public class security {
+    public static void main(String[] args) throws MalformedURLException {
+        security s = new security();
+        System.out.print(s.isValidURL("https:/www.example.com"));
+    }
+
+    boolean isValidURL(String url) throws MalformedURLException {
+        String[] schemes = {"http","https"};
+        UrlValidator validator = new UrlValidator(schemes);
+        return validator.isValid(url);
+    }
+
+}
